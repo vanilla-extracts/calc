a5cb008fb4b063a778546ee424ca3e2d93d72734
Charlotte Thomas <contact@nwa2coco.fr>
2024-05-19 11:53:26 +0200

    remove stack overflow on exponentiation

--- src/functions/expo.rs
+++ src/functions/expo.rs
@@ -1,11 +1,7 @@
-use crate::exact_math::rationals::Rationals;
-use crate::exact_math::symbolic::size;
-use crate::functions::add::add;
 use crate::functions::function::apply_operator;
 use crate::functions::function::apply_operator_reverse;
 use crate::parsing::ast::Parameters;
 use crate::parsing::ast::Parameters::*;
-use crate::utils::matrix_utils::*;
 use std::collections::HashMap;
 
 pub fn expo(
@@ -43,49 +39,70 @@ pub fn expo(
         (Bool(b), Parameters::Null) => Bool(b),
         (Parameters::Null, Bool(b)) => Bool(b),
         (Bool(b), Bool(b2)) => Bool(b && b2),
-        (Parameters::Identifier(s), Parameters::Identifier(s2)) => apply_operator(
-            Parameters::Identifier(s),
-            Parameters::Identifier(s2),
-            ram,
-            expo,
-        ),
-        (Parameters::Identifier(s), Parameters::Int(i)) => {
-            apply_operator(Parameters::Identifier(s), Parameters::Int(i), ram, expo)
-        }
-        (Parameters::Int(i), Parameters::Identifier(s)) => {
-            apply_operator_reverse(Parameters::Int(i), Parameters::Identifier(s), ram, expo)
-        }
-        (Parameters::Identifier(s), Parameters::Float(i)) => {
-            apply_operator(Parameters::Identifier(s), Parameters::Float(i), ram, expo)
-        }
+        (Parameters::Identifier(s), Parameters::Identifier(s2)) => match ram {
+            None => Parameters::Identifier(s),
+            Some(_) => apply_operator(
+                Parameters::Identifier(s),
+                Parameters::Identifier(s2),
+                ram,
+                expo,
+            ),
+        },
+        (Parameters::Identifier(s), Parameters::Int(i)) => match ram {
+            None => Parameters::Int(i),
+            Some(_) => apply_operator(Parameters::Identifier(s), Parameters::Int(i), ram, expo),
+        },
+        (Parameters::Int(i), Parameters::Identifier(s)) => match ram {
+            None => Parameters::Int(i),
+            Some(_) => {
+                apply_operator_reverse(Parameters::Int(i), Parameters::Identifier(s), ram, expo)
+            }
+        },
+        (Parameters::Identifier(s), Parameters::Float(i)) => match ram {
+            None => Parameters::Float(i),
+            Some(_) => apply_operator(Parameters::Identifier(s), Parameters::Float(i), ram, expo),
+        },
 
-        (Parameters::Rational(s), Parameters::Identifier(ss)) => apply_operator_reverse(
-            Parameters::Rational(s.clone()),
-            Parameters::Identifier(ss.clone()),
-            ram,
-            expo,
-        ),
-        (Parameters::Identifier(ss), Parameters::Rational(s)) => apply_operator(
-            Parameters::Identifier(ss),
-            Parameters::Rational(s),
-            ram,
-            expo,
-        ),
-        (Parameters::Identifier(s), Parameters::Null) => {
-            apply_operator(Parameters::Identifier(s), Parameters::Null, ram, expo)
-        }
-        (Parameters::Null, Parameters::Identifier(s)) => {
-            apply_operator(Parameters::Identifier(s), Parameters::Null, ram, expo)
-        }
-        (Parameters::Float(i), Parameters::Identifier(s)) => {
-            apply_operator_reverse(Parameters::Float(i), Parameters::Identifier(s), ram, expo)
-        }
-        (Bool(b), Parameters::Identifier(s)) => {
-            apply_operator_reverse(Bool(b), Parameters::Identifier(s), ram, expo)
-        }
-        (Parameters::Identifier(s), Bool(b)) => {
-            apply_operator(Parameters::Identifier(s), Bool(b), ram, expo)
-        }
+        (Parameters::Rational(s), Parameters::Identifier(ss)) => match ram {
+            None => Parameters::Rational(s),
+            Some(_) => apply_operator_reverse(
+                Parameters::Rational(s.clone()),
+                Parameters::Identifier(ss.clone()),
+                ram,
+                expo,
+            ),
+        },
+        (Parameters::Identifier(ss), Parameters::Rational(s)) => match ram {
+            None => Rational(s),
+            Some(_) => apply_operator(
+                Parameters::Identifier(ss),
+                Parameters::Rational(s),
+                ram,
+                expo,
+            ),
+        },
+        (Parameters::Identifier(s), Parameters::Null) => match ram {
+            None => Parameters::Null,
+            Some(_) => apply_operator(Parameters::Identifier(s), Parameters::Null, ram, expo),
+        },
+        (Parameters::Null, Parameters::Identifier(s)) => match ram {
+            None => Null,
+            Some(_) => apply_operator(Parameters::Identifier(s), Parameters::Null, ram, expo),
+        },
+        (Parameters::Float(i), Parameters::Identifier(s)) => match ram {
+            None => Float(i),
+            Some(_) => {
+                apply_operator_reverse(Parameters::Float(i), Parameters::Identifier(s), ram, expo)
+            }
+        },
+        (Bool(b), Parameters::Identifier(s)) => match ram {
+            None => Bool(b),
+            Some(_) => apply_operator_reverse(Bool(b), Parameters::Identifier(s), ram, expo),
+        },
+        (Parameters::Identifier(s), Bool(b)) => match ram {
+            None => Bool(b),
+            Some(_) => apply_operator(Parameters::Identifier(s), Bool(b), ram, expo),
+        },
 
         _ => Parameters::Identifier(
             "@Those two values are incompatible with the ^ operator".to_string(),
