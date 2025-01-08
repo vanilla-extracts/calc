33137e6d55425e8952a9d5787c6e334730ddbf87
Charlotte Thomas <me@charlotte-thomas.me>
2025-01-08 21:00:49 +0100

    bump to 3.3.5

--- src/configuration/loader.rs
+++ src/configuration/loader.rs
@@ -131,7 +131,7 @@ pub fn load_color(string: String) -> Color {
 
 pub fn replace_variable(str: String) -> String {
     str.replace("%author%", "Charlotte Thomas")
-        .replace("%version%", "v3.3.4")
+        .replace("%version%", "v3.3.5")
         .to_string()
 }
 
