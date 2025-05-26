7b84c3e638fefa0a7c9a929a1e4fd5512a7f643f
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-26 20:07:25 +0200

    version: bump to v3.5.0-beta

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ mod parsing;
 mod utils;
 
 thread_local! {static FLOAT_MODE: RefCell<FloatMode> = const {RefCell::new(FloatMode::Exact)}}
-static VERSION: &str = "v3.5.0-alpha";
+static VERSION: &str = "v3.5.0-beta";
 
 fn show_config(config: Config) -> (String, Option<Config>) {
     let loaded = load_config(config.clone());
