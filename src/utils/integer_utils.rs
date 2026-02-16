f860fc35e38462b5c05943b3c1d82143e2278d36
Charlotte Thomas <charlotte.thomas@dgfip.finances.gouv.fr>
2026-02-16 10:59:31 +0100

    documentation: harmonisation

--- src/utils/integer_utils.rs
+++ src/utils/integer_utils.rs
@@ -1,7 +1,7 @@
 /// # GCD
-/// Computes the GCD netween two mutable sixty four bits integers, returns the value as a sixty four bits integer.
+/// Computes the GCD netween two mutable sixty four bits integers.
 /// The algorithm is the classic euclid algorithm.
-///
+/// Returns the GCD
 pub fn gcd(mut n: i64, mut m: i64) -> i64 {
     while m != 0 {
         if m < n {
